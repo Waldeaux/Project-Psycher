db06b9c27468f4dd3eb7848074620b7a34dc5bb8
Waldeaux <tswalden95@gmail.com>
2020-12-07 00:45:10 -0600

    Add recalibration
    Currently testing. Can now switch to recalibration mode by holding h. Can rotate while in recalibration mode. Gravity is now applied by user's down direction. Camera also now goes above user by 10 units and points down

--- Project-Psycher/Assets/scripts/player/PlayerMovement.cs
+++ Project-Psycher/Assets/scripts/player/PlayerMovement.cs
@@ -7,9 +7,25 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
     public float speed;
     public float rotationSpeed;
+    public float recalibrateSpeed;
     public Camera cam;
     public float upperLimit;
     public float lowerLimit;
+
+    private Vector3 camPosition;
+    private Quaternion camRotation;
+
+    KeyCode recalibrateKey = KeyCode.H;
+
+    private enum PlayerState { 
+        normal,
+        recalibrate
+    }
+
+    private PlayerState currentState = PlayerState.normal;
+
+
+    private Vector3 storedVelocity;
 
     private void Start()
     {
@@ -22,12 +38,30 @@ public class PlayerMovement : MonoBehaviour
         {
             rotationSpeed = 1;
         }
+        if(recalibrateSpeed == 0)
+        {
+            recalibrateSpeed = 1;
+        }
     }
 
     private void Update()
     {
+        MoveCamera();
+        switch (currentState)
+        {
+            case (PlayerState.recalibrate):
+                Recalibrate();
+                break;
+            default:
+                NormalMovement();
+                break;
+        }
+    }
+
+    private void NormalMovement()
+    {
         Vector3 planarMovement = (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")) * speed;
-        Vector3 upMovement = Vector3.Project(rb.velocity, transform.up);
+        Vector3 upMovement = Vector3.Project(rb.velocity, transform.up) - transform.up * 9.8f * Time.deltaTime;
         rb.velocity = upMovement + planarMovement;
 
         rb.rotation *= Quaternion.AngleAxis(rotationSpeed * Input.GetAxis("Mouse X"), transform.up);
@@ -47,6 +81,41 @@ public class PlayerMovement : MonoBehaviour
                 cam.transform.localRotation = Quaternion.Euler(Mathf.Min(cam.transform.localRotation.eulerAngles.x, lowerLimit), 0, 0);
             }
         }
+        if (Input.GetKeyDown(recalibrateKey))
+        {
+            storedVelocity = rb.velocity;
+            rb.velocity = Vector3.zero;
+            currentState = PlayerState.recalibrate;
+            camPosition = new Vector3(0,10, 0);
+            camRotation = Quaternion.Euler(90, 0, 0);
+        }
+    }
+
+    private void Recalibrate()
+    {
+
+        if (!Input.GetKey(recalibrateKey))
+        {
+            rb.velocity = storedVelocity;
+            currentState = PlayerState.normal;
+            camPosition = Vector3.zero;
+            camRotation = Quaternion.Euler(Vector3.zero);
+            NormalMovement();
+            return;
+        }
+        Quaternion rotation = Quaternion.AngleAxis(recalibrateSpeed * Input.GetAxis("Vertical") * Time.deltaTime, transform.InverseTransformDirection(transform.right));
+            rotation *= Quaternion.AngleAxis(-recalibrateSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), transform.InverseTransformDirection(transform.forward));
+        transform.rotation *= rotation;
+    }
+
+    private void MoveCamera()
+    {
+        Vector3 differenceVector = (camPosition - cam.transform.localPosition);
+        cam.transform.localPosition += differenceVector.normalized * Mathf.Min(differenceVector.magnitude, 25*Time.deltaTime);
+
+        float xDifference = camRotation.eulerAngles.x - cam.transform.localRotation.eulerAngles.x;
+        float newX = cam.transform.localRotation.eulerAngles.x + Mathf.Min(xDifference, camRotation.eulerAngles.x * 25 * Time.deltaTime);
+        cam.transform.localRotation = Quaternion.Euler(newX, 0, 0);
     }
 
     
