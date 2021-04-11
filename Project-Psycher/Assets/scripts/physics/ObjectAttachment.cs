313400bd34b40c993b75eced78f225fe371e37f2
Waldeaux <tswalden95@gmail.com>
2021-04-11 01:33:58 -0500

    Update object attachment
    Set object's rigidbody to not use gravity and turn it on when a joint is destroyed

--- Project-Psycher/Assets/scripts/physics/ObjectAttachment.cs
+++ Project-Psycher/Assets/scripts/physics/ObjectAttachment.cs
@@ -19,22 +19,33 @@ public class ObjectAttachment : MonoBehaviour
             testCube.transform.position = transform.position + relativeOffset + transform.up * .05f + -transform.up * .1f;
         }
 
-        if(Physics.Raycast(transform.position+relativeOffset+transform.up*.05f, -transform.up, out hitInfo, .1f))
+        if (Physics.Raycast(transform.position + relativeOffset + transform.up * .05f, -transform.up, out hitInfo))
         {
             if (debug)
             {
                 print("good");
             }
-            transform.position = hitInfo.point - relativeOffset;
-            if (hitInfo.rigidbody)
+            Rigidbody rb = hitInfo.collider.gameObject.GetComponentInParent<Rigidbody>();
+            if (rb)
             {
+                GetComponent<Rigidbody>().useGravity = false;
                 FixedJoint joint = gameObject.AddComponent<FixedJoint>();
-                joint.connectedBody = hitInfo.rigidbody;
+                joint.connectedBody = rb;
                 if (breakforce > 0)
                 {
                     joint.breakForce = breakforce;
                 }
             }
+        }
+    }
+
+    private void OnJointBreak(float breakForce)
+    {
+        print(breakForce);
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb)
+        {
+            rb.useGravity = true;
         }
     }
 
